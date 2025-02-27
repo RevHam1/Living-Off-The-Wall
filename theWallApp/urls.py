51a827a2c91125256b76253c6d48d9325fd5801f
RevHam1 <36148347+RevHam1@users.noreply.github.com>
2025-02-27 06:59:07 -0600

    first commit

---
+++ theWallApp/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('register', views.register),
+    path('login', views.login),
+    path('success', views.success),
+    path('logout', views.logout),
+
+    ## Wall FUNCTIONALITY ##
+    path('create_message', views.create_mess),
+    path('create_comment', views.create_comm),
+    path('user/<int:user_id>', views.profile),
+    # path('like/<int:id>', views.add_like),
+    path('delete/<int:mess_id>', views.delete_mess),
+    path('comm_delete/<int:comm_id>', views.delete_comm),
+]
---
+++ theWallApp/views.py
@@ -0,0 +1,117 @@
+import bcrypt
+from django.contrib import messages
+from django.shortcuts import redirect, render
+
+from .models import Comment, User, Wall_Message
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+## Register, Login, Logout FUNCTIONALITIES ##
+def register(request):
+    if request.method == "POST":
+        errors = User.objects.create_validator(request.POST)
+        if errors:
+            for value in errors.values():
+                messages.error(request, value)
+            return redirect('/')
+        else:
+            hashed_pw = bcrypt.hashpw(
+                request.POST['password'].encode(), bcrypt.gensalt()).decode()
+            user = User.objects.create(
+                first_name=request.POST['first_name'],
+                last_name=request.POST['last_name'],
+                email=request.POST['email'],
+                birthday=request.POST['birthday'],
+                password=hashed_pw
+            )
+            request.session['user_id'] = user.id
+            return redirect('/success')
+    return redirect('/')
+
+
+def login(request):
+    if request.method == "POST":
+        user = User.objects.filter(email=request.POST['email'])
+        if user:
+            user = user[0]
+            if bcrypt.checkpw(request.POST['password'].encode(), user.password.encode()):
+                request.session['user_id'] = user.id
+                return redirect('/success')
+        messages.error(request, "Email or password is incorrect")
+    return redirect('/')
+
+
+def success(request):
+    if 'user_id' not in request.session:
+        messages.error(request, "You need to register or login!")
+        return redirect('/')
+
+    context = {
+        'user': User.objects.get(id=request.session['user_id']),
+        'all_messages': Wall_Message.objects.all(),
+    }
+    return render(request, "success.html", context)
+
+
+def logout(request):
+    request.session.flush()
+    # request.session.clear()
+    return redirect('/')
+
+
+## Wall FUNCTIONALITY ##
+# CREATE Post, Comment
+def create_mess(request):
+    if request.method == 'POST':
+        error = Wall_Message.objects.mess_validator(request.POST)
+        if error:
+            messages.error(request, error)
+            return redirect('/success')
+        new_mess = Wall_Message.objects.create(
+            content=request.POST['content'],
+            poster=User.objects.get(id=request.session['user_id'])
+        )
+        print(new_mess)
+        return redirect('/success')
+    return redirect('/')
+
+
+def create_comm(request):
+    if request.method == 'POST':
+        Comment.objects.create(
+            content=request.POST['content'],
+            poster=User.objects.get(id=request.session['user_id']),
+            wall_message=Wall_Message.objects.get(id=request.POST['message']),
+        )
+        return redirect('/success')
+    return redirect('/')
+
+# READ POST
+
+
+def profile(request, user_id):
+    context = {
+        'user': User.objects.get(id=user_id)
+    }
+    return render(request, "profile.html", context)
+
+# def add_like(request, id):
+#     liked_message = Wall_Message.objects.get(id=id)
+#     user_liking = User.objects.get(id=request.session['id'])
+#     liked_message.user_likes.add(user_liking)
+#     return redirect('/success')
+
+# DESTROY
+
+
+def delete_mess(request, mess_id):
+    Wall_Message.objects.get(id=mess_id).delete()
+    return redirect('/success')
+
+
+def delete_comm(request, comm_id):
+    Comment.objects.get(id=comm_id).delete()
+    return redirect('/success')
